84a382cc47118e6f464e244c7bc6eab2e2a29210
Murilo Russo <61585595+Dev-Russo@users.noreply.github.com>
2026-03-18 22:37:52 -0300

    Fix and Feats: Alembic, Config, Database, User

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 """
 Settings class to load environment variables from .env file. This class uses pydantic to validate the types of the variables and to provide default values if needed. The settings are loaded from the .env file located in the parent directory of the config.py file. The settings can be accessed using the settings variable, which is an instance of the Settings class.
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
 
-    class Config:
-        env_file = "../.env"
+    # Forma recomendada para Pydantic v2
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding='utf-8', extra='ignore')
 
 settings = Settings()
--- backend/database.py
+++ backend/database.py
@@ -1,4 +1,4 @@
-from config import settings
+from backend.config import settings
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -7,6 +7,7 @@ from sqlalchemy.orm import sessionmaker
 This module is responsible for setting up the database connection and creating the necessary objects for interacting with the database. It uses SQLAlchemy to create an engine, a session local, and a base class for the models. The database URL is loaded from the environment variables using the settings object from the config module. The engine is created using the create_engine function from SQLAlchemy, and the session local is created using the sessionmaker function. The base class is created using the declarative_base function, which allows us to define our models as classes that inherit from this base class. This module is essential for the proper functioning of the application, as it provides the necessary tools for interacting with the database and performing CRUD operations on the models.
 """
 
+print(f"DEBUG: Conectando em -> {settings.DATABASE_URL}") 
 engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
