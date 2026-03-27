7ccf05f4ad5d39c97d0e92beaa4eca03731fa05b
Murilo Russo <61585595+Dev-Russo@users.noreply.github.com>
2026-03-26 21:11:56 -0300

    fix: Model Atualizado

--- backend/models/sales.py
+++ backend/models/sales.py
@@ -1,16 +1,18 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, func, DateTime
 from sqlalchemy.orm import relationship
-from sqlalchemy.sql import func 
 from backend.database import Base
 
 class Sales(Base):
-    __tableneme__ = "sales"
+    __tablename__ = "sales"
 
     id = Column(Integer, primary_key=True, index=True)
     buyer_name = Column(String, index=True, nullable=False)
+    buyer_email = Column(String, index=True, nullable=False)
     price = Column(Integer, nullable=False)
     product_id = Column(Integer, ForeignKey("product.id"), nullable=False)
     method_of_payment = Column(String, nullable=False)
+    status = Column(String, index=True, default="")
+    sale_date = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
 
     #relationship with product
     product = relationship("Product", back_populates="sales")
