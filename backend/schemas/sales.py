e809d2dbf0e70ceae1436f1057413e7acc148e21
Murilo Russo <61585595+Dev-Russo@users.noreply.github.com>
2026-03-26 21:16:22 -0300

    Validator

--- backend/schemas/sales.py
+++ backend/schemas/sales.py
@@ -1,12 +1,21 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 from typing import Optional
-
+from datetime import datetime
 
 class SalesBase(BaseModel):
     buyer_name: str
     price: int
     product_id: int
     method_of_payment: str
+    sale_date: datetime = datetime.now()
+    status: str
+
+    @validator("status")
+    def validate_status(cls, value):
+        allowed_statuses = ["Payed", "Cancelled", "Check-in"]
+        if value not in allowed_statuses:
+            raise ValueError(f"Status must be one of {allowed_statuses}")
+        return value
 
 class SalesCreate(SalesBase):
     pass
@@ -16,6 +25,7 @@ class SalesUpdate(SalesBase):
     price: Optional[int] = None
     product_id: Optional[int] = None
     method_of_payment: Optional[str] = None
+    status: Optional[str] = None
 
 class SalesResponse(SalesBase):
     id: int
