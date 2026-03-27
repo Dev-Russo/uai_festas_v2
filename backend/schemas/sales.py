173a5b83ad717813326dc9c12f795b6e74540c9a
Murilo Russo <61585595+Dev-Russo@users.noreply.github.com>
2026-03-26 21:43:45 -0300

    fix: Validator Remove

--- backend/schemas/sales.py
+++ backend/schemas/sales.py
@@ -1,28 +1,23 @@
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, Field, validator, EmailStr
 from typing import Optional
 from datetime import datetime
 from backend.enums.sales import SaleStatus
 
 class SalesBase(BaseModel):
     buyer_name: str
+    buyer_email: EmailStr
     price: int
     product_id: int
     method_of_payment: str
-    sale_date: datetime = datetime.now()
+    sale_date: datetime = Field(default_factory=datetime.utcnow)
     status: SaleStatus
-
-    @validator("status")
-    def validate_status(cls, value):
-        allowed_statuses = [status.value for status in SaleStatus]
-        if value not in allowed_statuses:
-            raise ValueError(f"Status must be one of {allowed_statuses}")
-        return value
 
 class SalesCreate(SalesBase):
     pass
 
-class SalesUpdate(SalesBase):
+class SalesUpdate(BaseModel):
     buyer_name: Optional[str] = None
+    buyer_email: Optional[EmailStr] = None
     price: Optional[int] = None
     product_id: Optional[int] = None
     method_of_payment: Optional[str] = None
